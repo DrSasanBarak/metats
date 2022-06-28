11748cd07df25a8abcc651f1256d8390f08897bd
Amirabbas Asadi <amir137825@gmail.com>
2022-06-28 22:16:13 +0430

    adding meta-feature reduction to pipeline

--- metats/pipeline.py
+++ metats/pipeline.py
@@ -1,18 +1,37 @@
 import numpy as np
 from metats.forecasters.utils import is_sktime_forecaster, is_darts_forecaster
 from metats.forecasters.wrappers import DartsForecasterWrapper, SKTimeForecasterWrapper
+from sklearn.decomposition import PCA
 
 class MetaLearning():
     """
     Base Class for Meta-Learning Pipelines
     """
-    def __init__(self, method='averaging', loss='mse', metalearning_split=7):
+    def __init__(self, method='averaging', loss='mse', reduction='none', reduction_dim=64):
+        """
+        Args:
+            method: can be either 'averaging' or 'selection'. in the case of averagin,
+                    the predictions of base-forecaster will be combined by weighting
+                    based on classification weights. In the 'selection' mode, the best
+                    forecaster will be selected and others will be ingored
+            loss: loss function for generating meta-learning labels, can be 'mse', 'mae'.
+                  to use a custom loss function you can also pass a callable object.
+            reduction: you can apply dimensionality reduction on generated features.
+                       can be 'none', 'pca'. use reduction dim to specify
+                       final dimension.
+            reduction_dim: will be ignored if reduction='none'.
+                   
+        """
         self.method = method
         self.feature_generators = []
         self.base_forecasters = []
         self.meta_learner = None
         self.loss = loss
-        self.metalearning_split = metalearning_split
+        self.reduction = reduction
+        self.reduction_dim = reduction_dim
+
+        if self.reduction == 'pca':
+            self.reducer = PCA(n_components=self.reduction_dim)
     
     def add_feature(self, feature_generator):
         """
@@ -121,11 +140,28 @@ class MetaLearning():
         """
         self.meta_learner = metalearner
 
+    def reduce_meta_features(self, meta_features, prediction=False):
+        """
+        applying dimensionality reduction on the generated features 
+        Args:
+            meta_features: numpy array of meta-features (num_series, features_dim)
+            prediction: if True the stored features from training stage will be used
+        """
+        num_training_series = 0
+        if prediction:
+            num_training_series = self.training_features.shape[0] 
+            # concatenating the training features
+            meta_features = np.vstack([self.training_features, meta_features])
+        reduced = self.reducer.fit_transform(meta_features)[num_training_series:, :]
+        return reduced
+
     def fit(self, Y, fh, forecast_dim=0):
         """
         Fit the meta-learner
         Args:
             Y: the timeseries (numpy array) (num_series x series_length x covariates_dim)
+            fh: forecasting horizon
+            forecast_dim: forecasting dimension
         """
         # data spliting for meta-learning
         Y_true = Y[:, -fh:, forecast_dim]
@@ -134,6 +170,11 @@ class MetaLearning():
         meta_features = self.generate_features(Y[:, :-fh, :])
         predictions = self.generate_predictions(Y[:, :-fh, :], fh, forecast_dim=forecast_dim)
         labels = self.generate_labels(Y_true, predictions)
+        
+        # saving the generated features for using in prediction phase
+        if self.reduction != 'none':
+            self.training_features = np.copy(meta_features)
+            meta_features = self.reduce_meta_features(meta_features)
 
         self.meta_learner.fit(X=meta_features, y=labels)
 
@@ -182,9 +223,13 @@ class MetaLearning():
             forecast_dim: the dimension of the variable to be forecasted
         """
         meta_features = self.generate_features(Y[:, :-self.features_fh, :])
+
+        if self.reduction != 'none':
+            meta_features = self.reduce_meta_features(meta_features, prediction=True)
+
         predictions = self.generate_predictions(Y, fh, forecast_dim=forecast_dim)
         weights = self.predict_generate_weights(meta_features)
-        
+
         if self.method == 'averaging':
             result = self.averaging_predictions(weights, predictions)
         else:
