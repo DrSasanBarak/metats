a252d14783ded83f75407f2334d52a87bc8d7cb5
Amirabbas Asadi <amir137825@gmail.com>
2022-06-29 18:34:32 +0430

    index page for documentation

---
+++ docs/assets/scripts/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    hljs.highlightAll();
+    $('.navbar-burger').click(function() {
+        $('.navbar-burger').toggleClass('is-active');
+        $('.navbar-menu').toggleClass('is-active');
+    });
+});
